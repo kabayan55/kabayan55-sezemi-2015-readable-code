ccfd7ed3dbe6605c607208a9ff14f062ae07b79b
kabayan55 <g1120513@is.ocha.ac.jp>
2015-06-06 11:20:44 +0900

    オムライスをテキストファイルに出力
    出力（オムライス）をreceipe_data.txtに出力

--- recipe.py
+++ recipe.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-print 'オムライス'
+import sys
+import codecs
+import os
+
+recipe_data_txtfile=codecs.open(os.path.dirname(os.path.abspath(__file__))+'/receipe_data.txt','w')
+recipe_data_txtfile.write('オムライス'+'\n')
+recipe_data_txtfile.close()
