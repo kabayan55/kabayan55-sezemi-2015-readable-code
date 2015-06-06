5b53c919811bcc719a03770e3e1f5c4c54e232f7
kabayan55 <g1120513@is.ocha.ac.jp>
2015-06-06 12:02:47 +0900

    idつき3レシピ出力
    辞書にレシピを入れておく。
    idつきでレシピを出力

--- recipe.py
+++ recipe.py
@@ -5,9 +5,11 @@ import sys
 import codecs
 import os
 
+
+
 three_recipes2txtfile=open(sys.argv[1],'w')
 #three_recipes2txtfile=codecs.open(os.path.dirname(os.path.abspath(__file__))+'/three-recipes.txt','w')
-three_recipes2txtfile.write('オムライス'+'\n')
-three_recipes2txtfile.write('親子丼'+'\n')
-three_recipes2txtfile.write('杏仁豆腐'+'\n')
+three_recipes={1:'オムライス',2:'親子丼',3:'杏仁豆腐'}
+for recipe_key, recipe_name in three_recipes.items():
+  three_recipes2txtfile.write(str(recipe_key)+': '+ recipe_name +'\n')  
 three_recipes2txtfile.close()
