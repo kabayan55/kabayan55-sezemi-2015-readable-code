511e284dc5997e6e84ea330c0fb2ec9c778222f4
takuti <k.takuti@gmail.com>
2015-06-06 14:34:22 +0900

    spec5: change output destination

--- readme.md
+++ readme.md
@@ -1,7 +1,13 @@
 # 開発言語
 * Python
+
 ## 実行手順
-* python recipe.py [出力したいファイル名.txt]
+* python recipe.py
+
+## 入力データフォーマット
+
+[元の仕様](https://github.com/clear-code/sezemi-2015/blob/master/readable-code/task.md)ではファイルにデータが格納されているが、レシピ数やパラメータが少ないのでこのプログラムではコード内で直にデータを宣言
 
 ## 出力データフォーマット
-* テキストファイル
+
+標準出力
--- recipe.py
+++ recipe.py
@@ -7,9 +7,6 @@ import os
 
 
 
-three_recipes2txtfile=open(sys.argv[1],'w')
-#three_recipes2txtfile=codecs.open(os.path.dirname(os.path.abspath(__file__))+'/three-recipes.txt','w')
 three_recipes={1:'オムライス',2:'親子丼',3:'杏仁豆腐'}
 for recipe_key, recipe_name in three_recipes.items():
-  three_recipes2txtfile.write(str(recipe_key)+': '+ recipe_name +'\n')  
-three_recipes2txtfile.close()
+  print str(recipe_key)+': '+ recipe_name
