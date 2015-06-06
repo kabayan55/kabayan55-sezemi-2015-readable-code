2c0d50c698e9aca55c3d60c23fced8ba21650d96
kabayan55 <g1120513@is.ocha.ac.jp>
2015-06-06 11:31:09 +0900

    3レシピをtxtファイルに出力
    オムライス、親子丼、杏仁豆腐を改行して出力

--- recipe.py
+++ recipe.py
@@ -5,6 +5,8 @@ import sys
 import codecs
 import os
 
-recipe_data_txtfile=codecs.open(os.path.dirname(os.path.abspath(__file__))+'/receipe_data.txt','w')
-recipe_data_txtfile.write('オムライス'+'\n')
-recipe_data_txtfile.close()
+three_recipes2txtfile=codecs.open(os.path.dirname(os.path.abspath(__file__))+'/three-recipes.txt','w')
+three_recipes2txtfile.write('オムライス'+'\n')
+three_recipes2txtfile.write('親子丼'+'\n')
+three_recipes2txtfile.write('杏仁豆腐'+'\n')
+three_recipes2txtfile.close()
