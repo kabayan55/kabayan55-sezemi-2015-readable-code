f03eaa4005dc19067ec4b1dc8e31d05aa17498e0
takuti <k.takuti@gmail.com>
2015-06-06 15:08:05 +0900

    spec7: done implementation

--- recipe.py
+++ recipe.py
@@ -6,18 +6,22 @@ import codecs
 import os
 
 # Data is defined HERE
-three_recipes={1:'オムライス',2:'親子丼',3:'杏仁豆腐'}
+three_recipes={
+    1:('オムライス', 'http://cookpad.com/recipe/2653946'),
+    2:('親子丼',     'http://cookpad.com/recipe/2657882'),
+    3:('杏仁豆腐',   'http://cookpad.com/recipe/2654398')
+  }
 
-def printRecipe(recipe_id, recipe_name):
-  print str(recipe_id)+': '+ recipe_name
+def printRecipe(recipe_id, recipe_name, recipe_url):
+  print str(recipe_id)+': '+ recipe_name + ' ' + recipe_url
 
 def searchRecipeById(recipe_id):
-  searched_recipe_name = three_recipes[recipe_id]
-  printRecipe(recipe_id, searched_recipe_name)
+  searched_recipe_name, searched_recipe_url = three_recipes[recipe_id]
+  printRecipe(recipe_id, searched_recipe_name, searched_recipe_url)
 
 def printAllRecipes():
-  for recipe_id, recipe_name in three_recipes.items():
-    printRecipe(recipe_id, recipe_name)
+  for recipe_id, (recipe_name, recipe_url) in three_recipes.items():
+    printRecipe(recipe_id, recipe_name, recipe_url)
 
 if len(sys.argv) == 2:
   # Case: ID is given
