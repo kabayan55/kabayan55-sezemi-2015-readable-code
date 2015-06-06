f69e1f5d1e1f36d549502dcc5aeb624ca46b264c
takuti <k.takuti@gmail.com>
2015-06-06 14:48:33 +0900

    spec6: fix function name

--- recipe.py
+++ recipe.py
@@ -8,17 +8,17 @@ import os
 # Data is defined HERE
 three_recipes={1:'オムライス',2:'親子丼',3:'杏仁豆腐'}
 
-def searchById(recipe_id):
+def searchRecipeById(recipe_id):
   searched_recipe_name = three_recipes[recipe_id]
   print str(recipe_id)+': '+ searched_recipe_name
 
-def printAllRecipe():
+def printAllRecipes():
   for recipe_key, recipe_name in three_recipes.items():
     print str(recipe_key)+': '+ recipe_name
 
 if len(sys.argv) == 2:
   # Case: ID is given
-  searchById(int(sys.argv[1])) # assume that ID is correctly given
+  searchRecipeById(int(sys.argv[1])) # assume that ID is correctly given
 else:
   # Case: Normal output
-  printAllRecipe()
+  printAllRecipes()
